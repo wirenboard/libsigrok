fc933ef310fdac02885ff81c786d66ade74db07d
vzuevskiy <vladimir.zuevskiy@wirenboard.com>
2025-10-20 20:17:03 +0300

    fixup! wip

--- src/hardware/scpi-dmm/protocol.c
+++ src/hardware/scpi-dmm/protocol.c
@@ -608,7 +608,7 @@ SR_PRIV int scpi_dmm_owon_set_speed_from_text(const struct sr_dev_inst *sdi,
 
 	/* Send setup command */
 	scpi_dmm_cmd_delay(sdi->conn);
-	ret = sr_scpi_cmd(sdi->conn, devc->cmdset, 0, NULL,
+	ret = sr_scpi_cmd(sdi, devc->cmdset, 0, NULL,
 			  DMM_CMD_SETUP_SPEED, item->scpi_func_setup, param);
 	if (ret != SR_OK)
 		return ret;
